cb0e3db818653d9409ff5b47a9242afcd0b0841e
bs <bartosz.staniak@gmail.com>
2021-03-18 23:14:47 +0100

    added the tasks model class

---
+++ Models/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aWebASPapp.Models
+{
+    public class Pirates
+    {
+        public int Id { get; set; }
+        public String Person { get; set; }
+        public String Issue { get; set; }
+    }
+}
